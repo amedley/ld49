4a6bd6f55cad7562d27b36a130ca27c588138201
Andrew Gundlach <gundlachandrew@gmail.com>
2021-10-02 02:08:55 +0000

    last change before branching from master

--- game/source/html5/Html5.cs
+++ game/source/html5/Html5.cs
@@ -1,9 +1,11 @@
 using Godot;
 using System;
 
+// This is just here to make sure there is C# code which forces Godot to create scripts_metadata
+// we only need this source file if there is no other C# code in the game.
 public class Html5 : Node
 {
-    public override void _Ready()
-    {
-    }
+  public override void _Ready()
+  {
+  }
 }
